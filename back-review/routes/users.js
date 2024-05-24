1ffe1c1030dba7859afe39bad6c861478b6f27b7
kmaxcer <149956842+kmaxcer@users.noreply.github.com>
2024-05-24 15:48:45 +0300

    com
    Прошу посмотреть правильность кода, сейчас работаю с выгрузкой в облако.

---
+++ back-review/routes/users.js
@@ -0,0 +1,49 @@
+// Создаём роут для запросов пользователей
+const usersRouter = require("express").Router();
+
+// Импортируем вспомогательные функции
+const {
+  findAllUsers,
+  createUser,
+  findUserById,
+  updateUser,
+  deleteUser,
+  checkEmptyNameAndEmailAndPassword,
+  checkEmptyNameAndEmail,
+  checkIsUserExists,
+  hashPassword,
+  checkAuth,
+} = require("../middlewares/");
+const {
+  sendAllUsers,
+  sendUserCreated,
+  sendUserById,
+  sendUserUpdated,
+  sendUserDeleted,
+  sendMe,
+} = require("../controllers/");
+
+// Обрабатываем GET-запрос с роутом '/users'
+usersRouter.get("/users", findAllUsers, sendAllUsers);
+usersRouter.get("/users/:id", findUserById, sendUserById);
+usersRouter.get("/me", checkAuth, sendMe);
+usersRouter.post(
+  "/users",
+  findAllUsers,
+  checkIsUserExists,
+  checkEmptyNameAndEmailAndPassword,
+  checkAuth,
+  hashPassword,
+  createUser,
+  sendUserCreated
+);
+
+usersRouter.put(
+  "/users/:id",
+  checkEmptyNameAndEmail,
+  checkAuth,
+  updateUser,
+  sendUserUpdated
+);
+usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted);
+module.exports = usersRouter;
